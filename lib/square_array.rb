8eb2693e457d7228426f969f6ea148578be9e8e1
Diego Martinez <dmartinezm@gmail.com>
2019-09-26 02:34:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter=0
   array_len= array.length
   while counter<array_len do
-    array= array[counter]**2
+    array[counter]=array[counter]**2
     counter+=1
   end
   return array
